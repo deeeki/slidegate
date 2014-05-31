30dff72b2c7454c9ce1597f13188ef2fabbbcf9b
deeeki <deeeki@gmail.com>
2014-06-01 01:31:18 +0900

    Cast to appropriate types

--- lib/hateb/entry.rb
+++ lib/hateb/entry.rb
@@ -5,12 +5,12 @@ module Hateb
         anchor = element.at('a.entry-link')
         new({
           eid: element['data-eid'].to_i,
-          bookmark_count: element.at('li.users a span').text,
+          bookmark_count: element.at('li.users a span').text.to_i,
           entryrank: anchor['data-entryrank'].to_i,
           title: anchor['title'],
           url: anchor['href'].gsub(/\?.*/, ''),
           bookmark_url: "http://b.hatena.ne.jp#{element.at('a[data-track-click-target="entry"]')['href']}",
-          date: element.at('li.date').text,
+          date: Time.parse(element.at('li.date').text),
         })
       end
     end
