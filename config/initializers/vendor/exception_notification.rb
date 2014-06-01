31ba82a7960243fff0b1d762b77cca2e709e8ef0
deeeki <deeeki@gmail.com>
2014-06-02 01:44:55 +0900

    Configure exception_notification-rack

--- config/initializers/vendor/exception_notification.rb
+++ config/initializers/vendor/exception_notification.rb
@@ -1,8 +1,12 @@
 if Rails.env.production?
+  require 'exception_notification'
+
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
       email_prefix: '[SG] ',
       sender_address: %["exception_notifier" <noreply@slidegate.herokuapp.com>],
       exception_recipients: [ENV['MAIL_TO']]
     }
+
+  ExceptionNotifier::Rake.configure
 end
