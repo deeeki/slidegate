50c2b3d5e505a0be94e7cfe49a83e72ca5374090
deeeki <deeeki@gmail.com>
2014-06-07 19:18:52 +0900

    Link bookmark url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def daily_link_on date
     link_to l(date), daily_path(year: '%04d' % date.year, month: '%02d' % date.month, day: '%02d' % date.day)
   end
+
+  def bookmark_link slide
+    link_to slide.bookmark_url, class: 'users' do
+      raw "#{content_tag(:span, slide.bookmarks_count)}
+      #{slide.bookmarks_count > 1 ? 'users' : 'user'}"
+    end
+  end
 end
