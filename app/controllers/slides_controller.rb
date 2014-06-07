a4d093f78ca47759224dbe8956e95474fd8173d4
deeeki <deeeki@gmail.com>
2014-06-07 11:51:38 +0900

    Change recent limit

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -5,7 +5,7 @@ class SlidesController < ApplicationController
     if @date
       @slides = Slide.bookmarked_on(@date).order(bookmarks_count: :desc)
     else
-      @slides = Slide.order(bookmarked_at: :desc).limit(50)
+      @slides = Slide.order(bookmarked_at: :desc).limit(20)
       render :home
     end
   end
