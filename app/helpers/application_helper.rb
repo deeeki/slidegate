8e665814748282fd3c92c5dca9b20460e382c4d0
deeeki <deeeki@gmail.com>
2014-06-07 20:30:49 +0900

    Enhance daily pager

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,17 @@
 module ApplicationHelper
-  def daily_link_on date
-    link_to l(date), daily_path(year: '%04d' % date.year, month: '%02d' % date.month, day: '%02d' % date.day)
+  def daily_link_on date, rel: nil
+    label = l(date)
+    label = raw("&larr; #{label}") if rel == :previous
+    label = raw("#{label} &rarr;") if rel == :next
+    link_to label, daily_path(year: '%04d' % date.year, month: '%02d' % date.month, day: '%02d' % date.day), rel: rel
+  end
+
+  def daily_pager_on date
+    content_tag :ul, class: 'pager' do
+      pager = content_tag(:li, daily_link_on(date - 1.day, rel: :previous), class: 'previous')
+      pager << content_tag(:li, daily_link_on(date + 1.day, rel: :next), class: 'next') unless date == Date.today
+      raw pager
+    end
   end
 
   def bookmark_link slide
